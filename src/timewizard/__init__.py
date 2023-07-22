bcada38d7de0de06dba1a03fa44878935fb5bcbe
Jonah Pearl <jonahpearl@g.harvard.edu>
2023-07-22 12:32:39 -0400

    fix imports

--- src/timewizard/__init__.py
+++ src/timewizard/__init__.py
@@ -1,6 +1,10 @@
-from . import timeseries_utils
-from . import stats_utils
-from . import np_utils
-from .allenbrainobs import obs_utils
+# from . import timeseries_utils
+# from . import stats_utils
+# from . import np_utils
+# from .allenbrainobs import obs_utils
 
-__all__ = ['timeseries_utils', 'stats_utils', 'np_utils', 'obs_utils']
+# import all APIs into the top level (meta API)
+from .timeseries_utils import *
+from .stats_utils import *
+from .np_utils import *
+from .allenbrainobs.obs_utils import *
